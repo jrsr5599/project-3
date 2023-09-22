85c601cfbbd75e33f0b8321732e1b4c4d6517be1
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-21 19:15:37 -0700

    added some files

---
+++ left-on-scene/src/app.js
@@ -0,0 +1,12 @@
+import './app.css';
+import LeftonScene from './components/LeftonScene';
+
+function App() {
+  return (
+    <div className="LeftonScene">
+      <LeftonScene/>
+    </div>
+  );
+}
+
+export default App;
---
+++ left-on-scene/src/components/LeftonScene.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+
+function LeftonScene(){
+    return <LeftonScene/>
+}
+export default LeftonScene;
