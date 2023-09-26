78da35c1646ad523c4e5f5fc1a2664c351c910f7
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-25 20:49:28 -0700

    added nav bar

--- left-on-scene/src/app.js
+++ left-on-scene/src/app.js
@@ -1,70 +1,24 @@
 import React, { useState, useEffect } from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import MovieList from './components/MovieList';
+// import MovieList from './components/MovieList';
+import MovieSearch from './components/MovieSearch'
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavorites from './components/AddFavorites';
 import RemoveFavorites from './components/RemoveFavorites';
+import NavBar from './components/NavBar';
+// import 'dotenv/config'
+console.log (process.env.SECRET_KEY)
+// const key 
+
 
 const App = () => {
-	const [movies, setMovies] = useState([]);
-	const [searchValue, setSearchValue] = useState('');
-	const [favorites, setFavorites] = useState([]);
-
-	const getMovieRequest = async (searchValue) => {
-		const url = ` `; // need to add url
-
-		const response = await fetch(url);
-		const responseJson = await response.json();
-
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
-		}
-	};
-
-	const addFavoriteMovie = (movie) => {
-		const newFavoriteList = [...favorites, movie];
-		setFavorites(newFavoriteList);
-	};
-
-	const removeFavoriteMovie = (movie) => {
-		const newFavoriteList = favorites.filter(
-			(favorite) => favorite.imdbID !== movie.imdbID
-		);
-
-		setFavorites(newFavoriteList);
-	};
-
-	useEffect(() => {
-		getMovieRequest(searchValue);
-	}, [searchValue]);
-
 	return (
-		<div className='container-fluid movie-app'>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Movies' />
-				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-			</div>
-			<div className='row'>
-				<MovieList
-					movies={movies}
-					favoriteComponent={AddFavorites}
-					handleFavoritesClick={addFavoriteMovie}
-				/>
-			</div>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Favourites' />
-			</div>
-			<div className='row'>
-				<MovieList
-					movies={favorites}
-					handleFavoritesClick={removeFavoriteMovie}
-					favoriteComponent={RemoveFavorites}
-				/>
-			</div>
-		</div>
-	);
-};
+<div>
+	<MovieSearch/>
+	<NavBar/>
+	</div>
+	)};
 
 export default App;
--- left-on-scene/src/components/MovieSearch.js
+++ left-on-scene/src/components/MovieSearch.js
@@ -1,6 +1,12 @@
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../';
+import MovieList from '../components/MovieList';
+import MovieListHeading from '../components/MovieListHeading';
+import SearchBox from '../components/SearchBox';
+import AddFavorites from '../components/AddFavorites';
+import RemoveFavorites from '../components/RemoveFavorites';
+import axios from 'axios';
 
 
 // const MovieSearch = () => {
@@ -40,18 +46,78 @@
 
 
 
-const axios = require('axios');
-const options = {
-  method: 'GET',
-  url: 'https://moviesdatabase.p.rapidapi.com/titles',
-  headers: {
-    'X-RapidAPI-Key': '87e8bbac11msh2aca38f45e23940p123f6ajsn9d52274733fd',
-    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+// const axios = require('axios');
+// const options = {
+//   method: 'GET',
+//   url: 'https://moviesdatabase.p.rapidapi.com/titles',
+//   headers: {
+//     'X-RapidAPI-Key': '87e8bbac11msh2aca38f45e23940p123f6ajsn9d52274733fd',
+//     'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+//   }
+// };
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+const key = '15a6559706f656f8eadc5e7642675b96'
+
+const MovieSearch = () => {
+
+const [movies, setMovies] = useState([]);
+const [searchValue, setSearchValue] = useState('');
+const [favorites, setFavorites] = useState([]);
+const getMovieRequest = async (searchValue) => {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${searchValue}`; // need to add url
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  if (responseJson.Search) {
+    setMovies(responseJson.Search);
   }
 };
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+const addFavoriteMovie = (movie) => {
+  const newFavoriteList = [...favorites, movie];
+  setFavorites(newFavoriteList);
+};
+
+const removeFavoriteMovie = (movie) => {
+  const newFavoriteList = favorites.filter(
+    (favorite) => favorite.imdbID !== movie.imdbID
+  );
+
+  setFavorites(newFavoriteList);
+};
+
+useEffect(() => {
+  getMovieRequest(searchValue);
+}, [searchValue]);
+
+return (
+  <div className='container-fluid movie-app'>
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+      <MovieListHeading heading='Movies' />
+      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+    </div>
+    <div className='row'>
+      <MovieList
+        movies={movies}
+        favoriteComponent={AddFavorites}
+        handleFavoritesClick={addFavoriteMovie}
+      />
+    </div>
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+      <MovieListHeading heading='Favourites' />
+    </div>
+    <div className='row'>
+      <MovieList
+        movies={favorites}
+        handleFavoritesClick={removeFavoriteMovie}
+        favoriteComponent={RemoveFavorites}
+      />
+    </div>
+  </div>
+);}
+
+
+export default MovieSearch;
