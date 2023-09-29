17ecf09c2abbbf1c7915bdd04d18f6badfe72188
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-28 22:47:06 -0700

    added some changes

--- left-on-scene/src/components/MovieDisplay.js
+++ left-on-scene/src/components/MovieDisplay.js
@@ -1,15 +1,30 @@
 import React from "react";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 const MovieDisplay = ( {movie} ) => {
-    const moviePosterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-    return (
-        <div>
+    const moviePosterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+    const movieDetailUrl = `https://www.themoviedb.org/movie/${movie.id}`;
+    const containerStyle = {
+        border: "1px solid #ccc",
+        padding: "10px",
+        margin: "10px",
+        display: "inline-block",
+    };
+    return <FadeIn delay={500} transitionDuration={800}>
         <h1>{movie.title}</h1>
-        <a href="#" target="_blank">
-          <img src={moviePosterUrl} alt={movie.title} />
-        </a>
-      </div>
-    )
-}
+        <img src={moviePosterUrl} alt={movie.title} />
+        {/* <div style={containerStyle}>
+          <h1>
+            <a href={movieDetailUrl} target="_blank" rel="noopener noreferrer">
+              {movie.title}
+            </a>
+          </h1>
+          <a href={movieDetailUrl} target="_blank" rel="noopener noreferrer">
+            <img src={moviePosterUrl} alt={movie.title} />
+          </a>
+        </div> */}
+        </FadeIn>
+    }
+    
 
 export default MovieDisplay
---
+++ left-on-scene/src/components/singleMovie.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import MovieDisplay from '../components/MovieDisplay';
+const key = '15a6559706f656f8eadc5e7642675b96';
+
+const singleMovie =({ match }) => {
+    const [movie, setMovie] = useState(null);
+    const movieId = match.params.id;
+  
+    useEffect(() => {
+      const getMovieById = async () => {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}`;
+        const response = await fetch(url);
+        const responseJson = await response.json();
+        setMovie(responseJson);
+      };
+  
+      getMovieById();
+    }, [movieId]);
+  
+    if (!movie) {
+      return <div>Loading...</div>;
+    }
+  
+    return (
+      <div>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </div>
+    );
+  };
+  
+export default singleMovie
