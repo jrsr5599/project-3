38d04cdc22240e3296b74b9a114105d177db7423
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-28 22:50:35 -0700

    changed filename

--- left-on-scene/src/app.js
+++ left-on-scene/src/app.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './app.css';
 // import MovieList from './components/MovieList';
-import MovieSearch from './components/MovieSearch'
+import MovieSearch from './components/MovieSearch';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 // import RemoveFavorites from './components/RemoveFavorites';
@@ -11,23 +11,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Home from './components/LeftonScene';
 import Layout from './components/Layout';
-import LeftonScene from './components/LeftonScene'
-import singleMovie from './components/singleMovie';
-
+import LeftonScene from './components/LeftonScene';
+import SingleMovie from './components/SingleMovie';
 // import 'dotenv/config'
 // console.log (process.env.SECRET_KEY)
-// const key 
-
+// const key
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout /> }>
+        <Route path="/" element={<Layout />}>
           <Route index element={<LeftonScene />} />
-          <Route path="/Login" element={<Login />}/>
-          <Route path="/Movie/:movieid" element={<singleMovie/>} />
-		  <Route path="/Home" element={<MovieSearch />}/>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Movie/:movieid" element={<SingleMovie />} />
+          <Route path="/Home" element={<MovieSearch />} />
         </Route>
       </Routes>
     </Router>
